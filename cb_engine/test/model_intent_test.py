fdd304e5c4e979a8e96a767eb7952e1bc5d92045
ririariris <ehrkdrb1@gmail.com>
2023-06-16 16:24:26 +0900

    챗봇 fastapi

--- cb_engine/test/model_intent_test.py
+++ cb_engine/test/model_intent_test.py
@@ -4,7 +4,7 @@ from models.intent.intentModel import IntentModel
 p = Preprocessing(word2index_dic = 'cb_engine/train_tools/dict/chatbot_dict.bin', userdic='cb_engine/utils/user_dic.tsv')
 
 intent = IntentModel(model_name="cb_engine/models/intent/intent_model.h5", proprocess=p)
-query = "어린왕자 검색해줘"
+query = input("문장 입력 : ")
 predict = intent.predict_class(query)
 predict_label = intent.label[predict]
 
